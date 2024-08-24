735a3fa1ea8d5ffc6c01586b59be89ee0c2482e4
Cameron Bilyeu <158659699+magicat8@users.noreply.github.com>
2024-08-24 17:54:38 -0600

    Add files via upload

---
+++ math_test.py
@@ -0,0 +1,5 @@
+def is_palindrome(s):
+    if s == s[::-1]:
+        return s
+def bool_palindrome(s):
+    return s == s[::-1]
---
+++ palindrom_finder.py
@@ -0,0 +1,17 @@
+import time
+import math_test
+
+file = open("words.txt", 'r')
+open('palindromes.txt', 'w').close()
+new_file = open('palindromes.txt', 'a')
+line = file.readlines()
+
+
+for i in range(466549):
+    # time.sleep(1)
+    print(line[i].strip())
+    if math_test.bool_palindrome(line[i].strip()):
+        print('true')
+        print(f'{math_test.is_palindrome(line[i].strip())} is a palindrome')
+        palindrome = math_test.is_palindrome(line[i].strip())
+        new_file.write((f'{palindrome}\n'))
