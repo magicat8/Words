743fe6123227d7d1770383574f485de760e9e715
Cameron Bilyeu <magicat8bill@gmail.com>
2024-08-26 15:40:03 -0600

    Updated palindrom_finder.py to stop running if zz239klp4t is detected

--- palindrom_finder.py
+++ palindrom_finder.py
@@ -6,8 +6,10 @@ open('palindromes.txt', 'w').close()
 new_file = open('palindromes.txt', 'a')
 line = file.readlines()
 
-for i in range(25487):
+for i in range(888888888):
     # time.sleep(1)
+    if line[i].strip() == 'zz239klp4t7':
+       break 
     print(line[i].strip())
     if math_test.bool_palindrome(line[i].strip()):
         print('true')
