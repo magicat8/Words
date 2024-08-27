395837b77c3929ae723c22bbe8b9a10a2a833e64
Cameron Bilyeu <magicat8bill@gmail.com>
2024-08-27 13:48:24 -0600

    Added comments for readibility

---
+++ controle.py
@@ -0,0 +1,12 @@
+def is_palindrome(s):
+    if s == s[::-1]:
+        return s
+    
+def bool_palindrome(s):
+    return s == s[::-1]
+
+def dev_controles():
+    file = open("words.txt", 'a')
+    file.write('\nzz239klp4t77t4plk932zz')
+    file.close()
+
--- palindrom_finder.py
+++ palindrom_finder.py
@@ -1,18 +1,32 @@
 import time
-import math_test
+import controle
 
+# Opens word list to sort through
 file = open("words.txt", 'r')
+# Clears palindrome list
 open('palindromes.txt', 'w').close()
+# Opens palindrome list
 new_file = open('palindromes.txt', 'a')
+# Turns words.txt into a list
 line = file.readlines()
 
+# Selects each word in the list. Can handel files up to 888,888,888 words/lines long
 for i in range(888888888):
-    # time.sleep(1)
-    if line[i].strip() == 'zz239klp4t7':
-       break 
+    # Ends loop if the "password" is detected
+    if line[i].strip() == 'zz239klp4t77t4plk932zz':
+       break
+
+    # Prints each word in words.txt
     print(line[i].strip())
-    if math_test.bool_palindrome(line[i].strip()):
-        print('true')
-        print(f'{math_test.is_palindrome(line[i].strip())} is a palindrome')
-        palindrome = math_test.is_palindrome(line[i].strip())
+
+    # Checks if selected word is a palindrome
+    if controle.bool_palindrome(line[i].strip()):
+        print('True')
+
+        # Checks if selected word is a palindrome then returns the palindrome
+        print(f'{controle.is_palindrome(line[i].strip())} is a palindrome')
+    
+        # Assigns the palindrome to a variable
+        palindrome = controle.is_palindrome(line[i].strip())
+        # Adds the palindrome to the palindrome list
         new_file.write((f'{palindrome}\n'))
