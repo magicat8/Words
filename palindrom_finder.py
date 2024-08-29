ecdad2b0f79d25056435bb8d802e1501c4782ff6
Cameron Bilyeu <magicat8bill@gmail.com>
2024-08-29 11:52:52 -0600

    fixed errors added backwords.txt

--- palindrom_finder.py
+++ palindrom_finder.py
@@ -5,8 +5,16 @@ import controle
 file = open("words.txt", 'r')
 # Clears palindrome list
 open('palindromes.txt', 'w').close()
+# Clears reversed word list
+open('backwords_words.txt', 'w').close()
+# Clears compare list
+open('compare.txt', 'w').close()
 # Opens palindrome list
 new_file = open('palindromes.txt', 'a')
+# Opens reversed word list
+b = open('backwords_words.txt', 'a')
+# Opens compare list
+c = open('compare.txt', 'a')
 # Turns words.txt into a list
 line = file.readlines()
 
@@ -14,8 +22,11 @@ line = file.readlines()
 for i in range(888888888):
     # Ends loop if the "password" is detected
     if line[i].strip() == 'zz239klp4t77t4plk932zz':
+       backwords = line[i].strip()[::-1]
+       b.write(f'{backwords}\n')
        break
-
+    b.write(f'{backwords}\n')
+       break
     # Prints each word in words.txt
     print(line[i].strip())
 
@@ -29,4 +40,15 @@ for i in range(888888888):
         # Assigns the palindrome to a variable
         palindrome = controle.is_palindrome(line[i].strip())
         # Adds the palindrome to the palindrome list
-        new_file.write((f'{palindrome}\n'))
+        new_file.write(f'{palindrome}\n')
+b.close()
+b = open('backwords_words.txt', 'r')
+b_line = b.readlines()
+for z in range(888888888):
+    if b_line[z].strip() == 'zz239klp4t77t4plk932zz':
+       break
+
+    print(b_line[z].strip())
+
+    if b_line[z].strip() == line[z].strip():
+        c.write(f'{b_line[z].strip()}\n')
---
+++ pig_latin.py
@@ -0,0 +1,1 @@
+''' def constinints. split word'''
