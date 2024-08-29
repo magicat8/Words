a53db21884a79a3bd7a02a53a2708e1e1ccd30df
Cameron Bilyeu <magicat8bill@gmail.com>
2024-08-29 11:53:33 -0600

    fixed errors

--- palindrom_finder.py
+++ palindrom_finder.py
@@ -7,14 +7,10 @@ file = open("words.txt", 'r')
 open('palindromes.txt', 'w').close()
 # Clears reversed word list
 open('backwords_words.txt', 'w').close()
-# Clears compare list
-open('compare.txt', 'w').close()
 # Opens palindrome list
 new_file = open('palindromes.txt', 'a')
 # Opens reversed word list
 b = open('backwords_words.txt', 'a')
-# Opens compare list
-c = open('compare.txt', 'a')
 # Turns words.txt into a list
 line = file.readlines()
 
@@ -25,8 +21,8 @@ for i in range(888888888):
        backwords = line[i].strip()[::-1]
        b.write(f'{backwords}\n')
        break
+    backwords = line[i].strip()[::-1]
     b.write(f'{backwords}\n')
-       break
     # Prints each word in words.txt
     print(line[i].strip())
 
@@ -41,14 +37,3 @@ for i in range(888888888):
         palindrome = controle.is_palindrome(line[i].strip())
         # Adds the palindrome to the palindrome list
         new_file.write(f'{palindrome}\n')
-b.close()
-b = open('backwords_words.txt', 'r')
-b_line = b.readlines()
-for z in range(888888888):
-    if b_line[z].strip() == 'zz239klp4t77t4plk932zz':
-       break
-
-    print(b_line[z].strip())
-
-    if b_line[z].strip() == line[z].strip():
-        c.write(f'{b_line[z].strip()}\n')
