837f172b3547f3c4ea5dedb3d814f3be6260ee70
Koen Kleifgen <Corn.Bunny96@gmail.com>
2024-09-11 15:16:03 -0600

    removed auto clicker code

--- autoclicker.py
+++
@@ -1,7 +0,0 @@
-import pyautogui, time
-time.sleep(5)
-pyautogui.click()    # Click to make the window active. 
-pyautogui.FAILSAFE = False
-for i in range(100):
-    pyautogui.click()
-pyautogui.FAILSAFE = True
